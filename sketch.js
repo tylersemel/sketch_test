55432b5a91c6e583b473510c10646ced94bbf85a
tylersemel <129342949+tylersemel@users.noreply.github.com>
2024-01-22 17:04:22 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,12 @@
+var canvas;
+
+function setup() {
+  canvas = createCanvas(720, 400);
+  canvas.parent('sketch1');
+}
+
+
+function draw() {
+  background(20, 189, 100);
+  circle(224, 184, 220);
+}
